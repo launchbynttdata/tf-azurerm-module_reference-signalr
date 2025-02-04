33fce31748f692a2b1fd4c539e44d2cf1de67ef3
Erick Floresca <160937182+erick-floresca-nttd@users.noreply.github.com>
2025-02-04 15:06:53 -0500

    rename test functions

--- tests/post_deploy_functional/main_test.go
+++ tests/post_deploy_functional/main_test.go
@@ -25,7 +25,7 @@ const (
 	infraTFVarFileNameDefault        = "test.tfvars"
 )
 
-func TestSkeletonModule(t *testing.T) {
+func TestSignalRModule(t *testing.T) {
 
 	ctx := types.CreateTestContextBuilder().
 		SetTestConfig(&testimpl.ThisTFModuleConfig{}).
